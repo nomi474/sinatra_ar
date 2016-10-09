efe9ac1fff9412f437641ce8cf1589986057d090
Web Artist <nomi.sheikh@gmail.com>
2016-10-09 09:44:30 +0400

    Added edit user functionality and 'PUT' action in people_controller

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -13,7 +13,20 @@ require 'sinatra'
 		erb :"/people/show"
 	end
 	
+	get '/people/:id/edit' do
+		@person = Person.find(params[:id])
+		erb :'people/edit'		
+	end
 	
+	put '/people/:id' do
+		#get the record and update the first_name and last_name here...
+		person = Person.find(params[:id])
+		person.birthdate = params[:birthdate]
+		person.first_name = params[:first_name]
+		person.last_name = params[:last_name]
+		person.save
+		redirect "/people/#{person.id}"
+	end
 	post '/people' do
 		person = Person.create(first_name: params[:first_name], last_name: params[:last_name], 
 						birthdate: params[:birthdate])
